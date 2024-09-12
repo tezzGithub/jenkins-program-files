b4452c847c8884ef895db08f7a1fab757d8701cb
tezzGithub <168544232+tezzGithub@users.noreply.github.com>
2024-09-12 18:09:03 +0530

    initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("This is java program");
+        System.out.println("Output from java program");
+        System.out.println(new Date());
+        System.out.println("Hello");
+        System.out.println("This is jenkins project");
+    }
+}
