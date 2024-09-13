b2fa2e1945734dae09c2482f014ba2a744aaf8a6
tezzGithub <168544232+tezzGithub@users.noreply.github.com>
2024-09-13 12:11:38 +0530

    Added Student class

--- Main.java
+++ Main.java
@@ -7,5 +7,7 @@ public class Main{
         System.out.println(new Date());
         System.out.println("Hello");
        System.out.println("Added one more file");
+       Student student = new Student();
+       student.sayHello();
     }
 }
---
+++ Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    
+    public Student(){
+        System.out.println("Creating a Student object");
+    }
+
+    public void sayHello(){
+        System.out.println("Hii , I am Student");
+    }
+}
